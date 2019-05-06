956f8688accb76f306311e8af7f2229088597b88
Qarshyga Raymbek <karshiga.99@gmail.com>
2019-05-06 12:37:23 +0600

    snake to camel

--- src/clip.js
+++ src/clip.js
@@ -4,7 +4,7 @@ function clipLines(points, bounds) {
 
     const ix = bounds[i][1], iy = bounds[i][0];
     const kx = bounds[k][1], ky = bounds[k][0];
-    let new_points = [];
+    let newPoints = [];
     for (let z = 1; z < points.length; z++) {
       const j = z - 1;
 
@@ -12,30 +12,30 @@ function clipLines(points, bounds) {
       const zx = points[z][1], zy = points[z][0];
 
       const j_pos = (kx - ix) * (jy - iy) - (ky - iy) * (jx - ix);
-      const z_pos = (kx - ix) * (zy - iy) - (ky - iy) * (zx - ix);
-      if (j_pos >= 0 && z_pos < 0) {
-        new_points.push([y_intersect(ix, iy, kx, ky, jx, jy, zx, zy), x_intersect(ix, iy, kx, ky, jx, jy, zx, zy)]);
-      } else if (j_pos < 0 && z_pos >= 0) {
-        new_points.push([y_intersect(ix, iy, kx, ky, jx, jy, zx, zy), x_intersect(ix, iy, kx, ky, jx, jy, zx, zy)]);
-        new_points.push(points[z]);
-      } else if(j_pos >= 0 && z_pos >= 0) {
-        new_points.push(points[z]);
+      const zPos = (kx - ix) * (zy - iy) - (ky - iy) * (zx - ix);
+      if (j_pos >= 0 && zPos < 0) {
+        newPoints.push([intersectY(ix, iy, kx, ky, jx, jy, zx, zy), intersectX(ix, iy, kx, ky, jx, jy, zx, zy)]);
+      } else if (j_pos < 0 && zPos >= 0) {
+        newPoints.push([intersectY(ix, iy, kx, ky, jx, jy, zx, zy), intersectX(ix, iy, kx, ky, jx, jy, zx, zy)]);
+        newPoints.push(points[z]);
+      } else if(j_pos >= 0 && zPos >= 0) {
+        newPoints.push(points[z]);
       }
     }
-    new_points.push(new_points[0]);
-    points = new_points;
+    newPoints.push(newPoints[0]);
+    points = newPoints;
   }
   return points;
 }
 
-function x_intersect(x1, y1, x2, y2, x3, y3, x4, y4) {
+function intersectX(x1, y1, x2, y2, x3, y3, x4, y4) {
   const a = (x1 * y2 - y1 * x2) * (x3 - x4) - 
             (x1 - x2) * (x3 * y4 - y3 * x4);
   const b = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
   return a/b;
 }
 
-function y_intersect(x1, y1, x2, y2, x3, y3, x4, y4) {
+function intersectY(x1, y1, x2, y2, x3, y3, x4, y4) {
   const a = (x1 * y2 - y1 * x2) * (y3 - y4) - 
             (y1 - y2) * (x3 * y4 - y3 * x4); 
   const b = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4); 
